5c408cb2b3d4c9c0776acf17abe8637ef3e329a2
Emir <emir.kobaslic@gmail.com>
2018-02-26 18:33:01 +0000

    Initial commit

---
+++ util/Spotify.js
@@ -0,0 +1,42 @@
+const accessToken = '';
+const clientID = '8cf264b555644f03951d544eb2271163';
+const redirectUri = 'localhost:3000';
+
+Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+  },
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    },
+
+    search(term) {
+      return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      }).then(jsonResponse => {
+        return jsonResponse.tracks.items.map(track => ({
+         id: track.id,
+         name: track.name,
+         artist: track.artists[0].name,
+         album: track.album.name,
+         uri: track.uri
+       }));
+      })
+    }
+}
+
+export default Spotify;
